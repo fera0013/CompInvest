b9e846184ff6330866ae9150730c622e76ad2b9a
Ralph Fehrer <ralphfehrer@gmail.com>
2014-10-14 22:07:15 +0200

    Just enough to pass the Quiz for homework 3 of Computational Investment - caution: non-optimized code!!!

--- ComputationalInvestment/CIHomework3.py
+++ ComputationalInvestment/CIHomework3.py
@@ -5,7 +5,7 @@ import QSTK.qstkutil.DataAccess as da
 import datetime as dt
 import csv
 #step1
-reader=csv.reader(open("orders.csv",'rU'),delimiter=';')
+reader=csv.reader(open("orders2.csv",'rU'),delimiter=';')
 dates=[]
 symbols=[]
 for order in reader:
@@ -24,25 +24,39 @@ ldf_data = c_dataobj.get_data(ldt_timestamps, symbolList, ls_keys)
 d_data = dict(zip(ls_keys, ldf_data))
 #Step3
 tradeMatrix = pandas.DataFrame(index=ldt_timestamps,columns=symbolList).fillna(0)
-reader=csv.reader(open("orders.csv",'rU'),delimiter=';')
+reader=csv.reader(open("orders2.csv",'rU'),delimiter=';')
 for order in reader:
     orderInfo= order[0].split(',')
-    orderDate = dt.datetime(int( orderInfo[0]),int( orderInfo[1]),int(orderInfo[2]))
+    orderDate = dt.datetime(int(orderInfo[0]),int( orderInfo[1]),int(orderInfo[2]))
     for marketDay, numberTraded in tradeMatrix.iterrows():
         if marketDay._date_repr ==orderDate.strftime('%Y-%m-%d'):
             amount=0
             if  orderInfo[4]=='Buy':
                 amount = int(orderInfo[5])
             else:
-                amount = - int(orderInfo[5])
+                amount = -int(orderInfo[5])
             numberTraded[orderInfo[3]] = amount
-print tradeMatrix
 #step4 
 cash = pandas.Series(0, index=ldt_timestamps)
+cash[ldt_timestamps[0]]=1000000
 for marketDay, orderValues in tradeMatrix.iterrows():
-    cash.ix[marketDay]+=orderValues*d_data['close'].ix[marketDay]
-print cash
-i=1
+    cash.ix[marketDay]-=pandas.Series.sum(orderValues*d_data['close'].ix[marketDay])
+
+d_data['close']['CASH']=1.0
+
+tradeMatrix['CASH']=cash
+holdMatrix=tradeMatrix.cumsum()
+
+
+values = pandas.Series(0,index=ldt_timestamps)
+
+
+for valueDate,unused in tradeMatrix.iterrows():
+    values.ix[valueDate]=holdMatrix.ix[valueDate].T.dot(d_data['close'].ix[valueDate])
+
+pandas.set_option('display.max_rows', 10000)
+print values
+
 
 
 
