8b0243ca4c63c83c73f6aa43707ae1669d2e173d
Ralph Fehrer <ralphfehrer@gmail.com>
2014-10-14 16:38:46 +0200

    Implementation until step 4

--- Examples/Basic/tutorial1.py
+++ Examples/Basic/tutorial1.py
@@ -1,114 +1,48 @@
-'''
-(c) 2011, 2012 Georgia Tech Research Corporation
-This source code is released under the New BSD license.  Please see
-http://wiki.quantsoftware.org/index.php?title=QSTK_License
-for license details.
-
-Created on January, 24, 2013
-
-@author: Sourabh Bajaj
-@contact: sourabhbajaj@gatech.edu
-@summary: Example tutorial code.
-'''
-
-# QSTK Imports
+import pandas
 import QSTK.qstkutil.qsdateutil as du
 import QSTK.qstkutil.tsutil as tsu
 import QSTK.qstkutil.DataAccess as da
-
-# Third Party Imports
 import datetime as dt
-import matplotlib.pyplot as plt
-import pandas as pd
+import csv
+#step1
+reader=csv.reader(open("orders.csv",'rU'),delimiter=';')
+dates=[]
+symbols=[]
+for order in reader:
+    orderInfo= order[0].split(',')
+    dates.append(dt.datetime(int( orderInfo[0]),int( orderInfo[1]),int(orderInfo[2])))
+    symbols.append( orderInfo[3])
+symbolList = list(set( symbols))
+#step2
+dt_start = dates[0]
+dt_end = dates[-1]+dt.timedelta(days=1)
+dt_timeofday = dt.timedelta(hours=16)
+ldt_timestamps = du.getNYSEdays(dt_start, dt_end, dt_timeofday)
+c_dataobj = da.DataAccess('Yahoo')
+ls_keys = ['close']
+ldf_data = c_dataobj.get_data(ldt_timestamps, symbolList, ls_keys)
+d_data = dict(zip(ls_keys, ldf_data))
+#Step3
+tradeMatrix = pandas.DataFrame(index=ldt_timestamps,columns=symbolList).fillna(0)
+reader=csv.reader(open("orders.csv",'rU'),delimiter=';')
+for order in reader:
+    orderInfo= order[0].split(',')
+    orderDate = dt.datetime(int( orderInfo[0]),int( orderInfo[1]),int(orderInfo[2]))
+    for marketDay, numberTraded in tradeMatrix.iterrows():
+        if marketDay._date_repr ==orderDate.strftime('%Y-%m-%d'):
+            amount=0
+            if  orderInfo[4]=='Buy':
+                amount = int(orderInfo[5])
+            else:
+                amount = - int(orderInfo[5])
+            numberTraded[orderInfo[3]] = amount
+print tradeMatrix
+#step4 
+cash = pandas.Series(0, index=ldt_timestamps)
+for marketDay, orderValues in tradeMatrix.iterrows():
+    cash.ix[marketDay]+=orderValues*d_data['close'].ix[marketDay]
+print cash
+i=1
 
-print "Pandas Version", pd.__version__
 
 
-def main():
-    ''' Main Function'''
-
-    # List of symbols
-    ls_symbols = ["AAPL", "GLD", "GOOG", "$SPX", "XOM"]
-
-    # Start and End date of the charts
-    dt_start = dt.datetime(2006, 1, 1)
-    dt_end = dt.datetime(2010, 12, 31)
-
-    # We need closing prices so the timestamp should be hours=16.
-    dt_timeofday = dt.timedelta(hours=16)
-
-    # Get a list of trading days between the start and the end.
-    ldt_timestamps = du.getNYSEdays(dt_start, dt_end, dt_timeofday)
-
-    # Creating an object of the dataaccess class with Yahoo as the source.
-    c_dataobj = da.DataAccess('Yahoo')
-
-    # Keys to be read from the data, it is good to read everything in one go.
-    ls_keys = ['open', 'high', 'low', 'close', 'volume', 'actual_close']
-
-    # Reading the data, now d_data is a dictionary with the keys above.
-    # Timestamps and symbols are the ones that were specified before.
-    ldf_data = c_dataobj.get_data(ldt_timestamps, ls_symbols, ls_keys)
-    d_data = dict(zip(ls_keys, ldf_data))
-
-    # Filling the data for NAN
-    for s_key in ls_keys:
-        d_data[s_key] = d_data[s_key].fillna(method='ffill')
-        d_data[s_key] = d_data[s_key].fillna(method='bfill')
-        d_data[s_key] = d_data[s_key].fillna(1.0)
-
-    # Getting the numpy ndarray of close prices.
-    na_price = d_data['close'].values
-
-    # Plotting the prices with x-axis=timestamps
-    plt.clf()
-    plt.plot(ldt_timestamps, na_price)
-    plt.legend(ls_symbols)
-    plt.ylabel('Adjusted Close')
-    plt.xlabel('Date')
-    plt.savefig('adjustedclose.pdf', format='pdf')
-
-    # Normalizing the prices to start at 1 and see relative returns
-    na_normalized_price = na_price / na_price[0, :]
-
-    # Plotting the prices with x-axis=timestamps
-    plt.clf()
-    plt.plot(ldt_timestamps, na_normalized_price)
-    plt.legend(ls_symbols)
-    plt.ylabel('Normalized Close')
-    plt.xlabel('Date')
-    plt.savefig('normalized.pdf', format='pdf')
-
-    # Copy the normalized prices to a new ndarry to find returns.
-    na_rets = na_normalized_price.copy()
-
-    # Calculate the daily returns of the prices. (Inplace calculation)
-    # returnize0 works on ndarray and not dataframes.
-    tsu.returnize0(na_rets)
-
-    # Plotting the plot of daily returns
-    plt.clf()
-    plt.plot(ldt_timestamps[0:50], na_rets[0:50, 3])  # $SPX 50 days
-    plt.plot(ldt_timestamps[0:50], na_rets[0:50, 4])  # XOM 50 days
-    plt.axhline(y=0, color='r')
-    plt.legend(['$SPX', 'XOM'])
-    plt.ylabel('Daily Returns')
-    plt.xlabel('Date')
-    plt.savefig('rets.pdf', format='pdf')
-
-    # Plotting the scatter plot of daily returns between XOM VS $SPX
-    plt.clf()
-    plt.scatter(na_rets[:, 3], na_rets[:, 4], c='blue')
-    plt.ylabel('XOM')
-    plt.xlabel('$SPX')
-    plt.savefig('scatterSPXvXOM.pdf', format='pdf')
-
-    # Plotting the scatter plot of daily returns between $SPX VS GLD
-    plt.clf()
-    plt.scatter(na_rets[:, 3], na_rets[:, 1], c='blue')  # $SPX v GLD
-    plt.ylabel('GLD')
-    plt.xlabel('$SPX')
-    plt.savefig('scatterSPXvGLD.pdf', format='pdf')
-
-if __name__ == '__main__':
-    main()
