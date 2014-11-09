602c373b6ccd2e9c24a0a82c18788a72fc94b441
Ralph Fehrer <ralphfehrer@gmail.com>
2014-11-09 14:32:56 +0100

    ReadData signature refactured

--- ComputationalInvestment/PortfolioOptimizer.py
+++ ComputationalInvestment/PortfolioOptimizer.py
@@ -13,7 +13,7 @@ import time
 from scipy.optimize import minimize
 
 
-def ReadData(startDate, endDate, ls_symbols):
+def ReadData(startDate, endDate, ls_symbols,ls_data=['open', 'high', 'low', 'close', 'volume', 'actual_close'],str_dataSource='Yahoo'):
     #Create datetime objects for Start and End dates (STL)
 
     #Initialize daily timestamp: closing prices, so timestamp should be hours=16 (STL)
@@ -23,14 +23,11 @@ def ReadData(startDate, endDate, ls_symbols):
     ldt_timestamps = du.getNYSEdays(startDate, endDate, dt_timeofday);
 
     #Create an object of the QSTK-dataaccess class with Yahoo as the source (QSTK)
-    c_dataobj = da.DataAccess('Yahoo', cachestalltime=0);
-
-    #Keys to be read from the data
-    ls_keys = ['open', 'high', 'low', 'close', 'volume', 'actual_close'];
+    c_dataobj = da.DataAccess(str_dataSource, cachestalltime=0);
 
     #Read the data and map it to ls_keys via dict() (i.e. Hash Table structure)
-    ldf_data = c_dataobj.get_data(ldt_timestamps, ls_symbols, ls_keys);
-    d_data = dict(zip(ls_keys, ldf_data));
+    ldf_data = c_dataobj.get_data(ldt_timestamps, ls_symbols, ls_data);
+    d_data = dict(zip(ls_data, ldf_data));
 
     return [d_data, startDate, endDate, dt_timeofday, ldt_timestamps];
 
